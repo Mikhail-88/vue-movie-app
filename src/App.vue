a1fc16c1252402513942e72102e744a492b94fb6
Mikhail <mikhail.sapuzhak@gmail.com>
2020-05-31 13:58:50 +0300

    added pagination

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,58 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <PosterBg :poster="posterBg" />
+    <MoviesList :list='moviesList' @changePoster="onChangePoster" />
+    <Pagination
+      :currentPage='currentPage'
+      :perPage='moviesPerPage'
+      :total="moviesLength"
+      @pageChanged="onPageChanged"
+    />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
+import { mapActions, mapGetters } from 'vuex';
+import MoviesList from '@/components/MoviesList.vue';
+import PosterBg from '@/components/PosterBg.vue';
+import Pagination from '@/components/Pagination.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld,
+    MoviesList,
+    PosterBg,
+    Pagination,
+  },
+  data: () => ({
+    posterBg: '',
+  }),
+  computed: {
+    ...mapGetters('movies', [
+      'moviesList',
+      'currentPage',
+      'moviesPerPage',
+      'moviesLength',
+    ]),
+  },
+  watch: {
+    '$route.query': {
+      handler: 'onPageQueryChange',
+      immediate: true,
+      deep: true,
+    },
+  },
+  methods: {
+    ...mapActions('movies', ['changeCurrentPage']),
+    onPageQueryChange({ page = 1 }) {
+      this.changeCurrentPage(Number(page));
+    },
+    onChangePoster(poster) {
+      this.posterBg = poster;
+    },
+    onPageChanged(page) {
+      this.$router.push({ query: { page } });
+    },
   },
 };
 </script>
@@ -21,8 +62,6 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  position: relative;
 }
 </style>
---
+++ src/components/Pagination.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="movies-pagination d-flex justify-content-center">
+    <BPagination
+      v-model="currentPageModel"
+      :per-page="perPage"
+      :total-rows="total"
+      prev-text="Prev"
+      next-text="Next"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Pagination',
+  props: {
+    currentPage: {
+      type: Number,
+      default: 1,
+    },
+    total: {
+      type: Number,
+      default: 1,
+    },
+    perPage: {
+      type: Number,
+      default: 1,
+    },
+  },
+  computed: {
+    currentPageModel: {
+      get() {
+        return this.currentPage;
+      },
+      set(value) {
+        this.$emit('pageChanged', value);
+      },
+    },
+  },
+};
+</script>
+
+<style scoped>
+.movies-pagination {
+  margin-top: 30px;
+}
+.movies-pagination >>> .pagination .page-item .page-link {
+  background-color: transparent;
+  font-size: 12px;
+  color: white;
+  box-shadow: none;
+}
+.movies-pagination >>> .pagination .page-item.active .page-link {
+  border-color: white;
+  background-color: #fff;
+  color: black;
+}
+.movies-pagination >>> .pagination .page-item.disabled .page-link {
+  color: gray;
+}
+</style>
